44ac3c451b2c79d642bd4f111712b690f92df9bf
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-25 14:16:38 -0300

    adiciona configuração do google recaptcha para o MultipleLocalAuth

--- compose/production/config.d/authentication.php
+++ compose/production/config.d/authentication.php
@@ -11,30 +11,38 @@ return [
     'auth.config' => array(
         'salt' => env('AUTH_SALT', null),
         'timeout' => '24 hours',
+
+        'google-recaptcha-secret' => env('GOOGLE_RECAPTCHA_SECRET', false),
+        'google-recaptcha-sitekey' => env('GOOGLE_RECAPTCHA_SITEKEY', false),
+        
         'strategies' => [
            'Facebook' => array(
-               'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
-               'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
-               'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
+                'visible' => env('AUTH_FACEBOOK_VISIBLE', false),
+                'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
+                'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
+                'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
            ),
 
-            'LinkedIn' => array(
+           'LinkedIn' => array(
+                'visible' => env('AUTH_LINKEDIN_VISIBLE', null),
                 'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
                 'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
                 'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
             ),
             'Google' => array(
+                'visible' => env('AUTH_GOOGLE_VISIBLE', false),
                 'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
                 'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
                 'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
             ),
             'Twitter' => array(
+                'visible' => env('AUTH_TWITTER_VISIBLE', false),
                 'app_id' => env('AUTH_TWITTER_APP_ID', null),
                 'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
             ),
 
         ]
-    ),
+    )
 ];
