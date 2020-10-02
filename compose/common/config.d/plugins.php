92aa24420eddf358782f93467cdc92bd22f047e0
brmagrini@gmail.com <brmagrini@gmail.com>
2020-10-02 20:08:52 -0300

    Exemplos de config

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -12,6 +12,18 @@ return [
             'config' => [
                 'logotipo_instituicao' => '/assets/img/governo-cultura.png',
                 'logotipo_central' => '/assets/img/aldir-blanc.png',
+                'inciso1_enabled' => env('AB_INCISO1_ENABLE',true),
+                'inciso2_enabled' => env('AB_INCISO2_ENABLE',false),
+                'inciso3_enabled' => env('AB_INCISO3_ENABLE',false),
+                'project_id' => env('AB_INCISO2_PROJECT_ID',null),
+                'texto_home'=> env('AB_TEXTO_HOME','A Lei Aldir Blanc é fruto de forte mobilização social do campo artístico e cultural brasileiro, resultado de construção coletiva, a partir de webconferências nacionais e estaduais como plataformas políticas na formulação, articulação, tramitação e sanção presidencial.<br/><br/> Ela prevê o uso de 3 bilhões de reais para o auxílio de agentes da cultura atingidos pela pandemia da COVID-19. Investimentos para assegurar a preservação de toda a estrutura profissional e dinâmica de produção, criação, participação, preservação, formação e circulação dos bens e serviços culturais.<br/><br/> Clique no link abaixo para solicitar a renda emergencial como trabalhadora e trabalhador da cultura ou o subsídio para a manutenção de espaços artísticos e organizações culturais que tiveram as suas atividades interrompidas por força das medidas de isolamento social.'),
+                'botao_home'=> env('AB_BOTAO_HOME','Solicite seu auxilio'),
+                'titulo_home'=> env('AB_TITULO_HOME','Lei Aldir Blanc'),
+                'inciso1' => (array) json_decode(env('AB_INCISO1', '[]')),
+                'link_suporte' => env('AB_LINK_SUPORTE','   '),
+                'privacidade_termos_condicoes' => env('AB_PRIVACIDADE_TERMOS',null),
+                'inciso1_opportunity_id' => env('AB_INCISO1_OPPORTUNITY_ID', null),
+                'inciso2_opportunity_ids' => (array) json_decode(env('AB_INCISO2_OPPORTUNITY_IDS', '[]')),                
 
             ],
         ],
