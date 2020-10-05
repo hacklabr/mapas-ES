d2844e6646f3e6f2d8a58bf78c19353d872d165d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-10-04 23:45:40 -0300

    configuração de offline

---
+++ compose/common/config.d/offline.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'app.offline' => false,// date('Y-m-d H:i:s') < '2020-10-05 08:00:00',
+    'app.offlineUrl' => '/em-breve',
+    'app.offlineBypassFunction' => function() {
+        $senha = $_GET['online'] ?? '';
+        
+        if ($senha === 'aldir.blanc.es') {
+            $_SESSION['online'] = true;
+        }
+
+        return $_SESSION['online'] ?? false;
+    }
+];
