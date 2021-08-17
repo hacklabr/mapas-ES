12ba8d4b1337679142e713f54c39980971ab4c5a
erleibiazzio <erleibiazzio@gmail.com>
2021-08-17 14:15:07 -0300

    Remove textos e botoes da lei AldirBlanc da home

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -25,9 +25,9 @@ return [
                 'inciso1_opportunity_id' => 1,
                 'inciso2_opportunity_ids' => (array) json_decode(env('AB_INCISO2_OPPORTUNITY_IDS', '[]')),
                 
-                'texto_home'=> env('AB_TEXTO_HOME','A Lei Aldir Blanc é fruto de forte mobilização social do campo artístico e cultural brasileiro, resultado de construção coletiva, a partir de webconferências nacionais e estaduais como plataformas políticas na formulação, articulação, tramitação e sanção presidencial.<br/><br/> Ela prevê o uso de 3 bilhões de reais para o auxílio de agentes da cultura atingidos pela pandemia da COVID-19. Investimentos para assegurar a preservação de toda a estrutura profissional e dinâmica de produção, criação, participação, preservação, formação e circulação dos bens e serviços culturais.<br/><br/> Clique no link abaixo para solicitar a renda emergencial como trabalhadora e trabalhador da cultura ou o subsídio para a manutenção de espaços artísticos e organizações culturais que tiveram as suas atividades interrompidas por força das medidas de isolamento social.'),
-                'botao_home'=> env('AB_BOTAO_HOME','Solicite seu auxilio'),
-                'titulo_home'=> env('AB_TITULO_HOME','Lei Aldir Blanc'),
+                'texto_home'=> env('AB_TEXTO_HOME',null),
+                'botao_home'=> env('AB_BOTAO_HOME',null),
+                'titulo_home'=> env('AB_TITULO_HOME',null),
                 'link_suporte' => env('AB_LINK_SUPORTE','mailto:suportemapacultural@secult.es.gov.br'),
                 'privacidade_termos_condicoes' => env('AB_PRIVACIDADE_TERMOS',null),
                 'inciso2_default' => [
