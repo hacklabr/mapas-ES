ec5efd49d63b9ad0968b186d2d610512f1443684
erleibiazzio <erleibiazzio@gmail.com>
2021-08-17 15:29:02 -0300

    Altera logo

--- themes/EspiritoSanto/layouts/parts/header-logo.php
+++ themes/EspiritoSanto/layouts/parts/header-logo.php
@@ -1,1 +1,1 @@
-<h1 id="brand-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-site.png'); ?>" /></a></h1>
+<h1 id="brand-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-es.png'); ?>" /></a></h1>
