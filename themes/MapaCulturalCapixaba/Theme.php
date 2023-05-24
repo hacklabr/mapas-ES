37e22144d1b3f733710230161e9718d188dd7151
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-24 16:51:51 -0300

    tema MapaCulturalCapixaba baseado no BaseV2

---
+++ themes/MapaCulturalCapixaba/Theme.php
@@ -0,0 +1,11 @@
+<?php
+namespace MapaCulturalCapixaba;
+
+
+use MapasCulturais\App;
+
+class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
+    static function getThemeFolder() {
+        return __DIR__;
+    }
+}
