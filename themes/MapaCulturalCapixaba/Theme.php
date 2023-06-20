aa1a2897c40504cd576ec9f4832cd3819e5d60a1
LimaSystem <wolegario49@gmail.com>
2023-06-20 18:14:19 -0300

    Cria webmanifest para o carregamento do favicons

--- themes/MapaCulturalCapixaba/Theme.php
+++ themes/MapaCulturalCapixaba/Theme.php
@@ -8,4 +8,22 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
     static function getThemeFolder() {
         return __DIR__;
     }
+
+    function _init() {
+
+        parent::_init();
+        
+        $app = App::i();
+        
+         // Manifest do five icon
+        $app->hook('GET(site.webmanifest)', function() use ($app) {
+            /** @var \MapasCulturais\Controller $this */
+            $this->json([
+                'icons' => [
+                    [ 'src' => $app->view->asset('img/favicon-16x16.png', false), 'type' => 'image/png', 'sizes' => '16x16' ],
+                    [ 'src' => $app->view->asset('img/favicon-32x32.png', false), 'type' => 'image/png', 'sizes' => '32x32' ]
+                ],
+            ]);
+        });
+    }
 }
