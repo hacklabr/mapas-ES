194d184f10c3c1f6dd4db4669c2281df779abd36
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-09-11 19:28:37 -0300

    move os termos de uso para a nova pasta

--- docker/common/config.d/lgpd.php
+++ docker/common/config.d/lgpd.php
@@ -3,16 +3,20 @@
 return [
     'module.LGPD' => [
         'termsOfUsage'=>[
-            'title'=> 'Termos e Condições de Uso', 
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html')
+            'title'=> 'Termos e condições de uso', 
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/terms-of-usage.html'),
+            'buttonText' => 'Aceito os termos e condições de uso'
+
         ],
         'privacyPolicy' => [
-            'title'=>  'Política de Privacidade',
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html')
+            'title'=>  'Política de privacidade',
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/privacy-policy.html'),
+            'buttonText' => 'Aceito a política de privacidade'
         ],
         'imageOfUsage' => [
             'title'=>  'Autorização de uso de imagem',
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/images-use.html')
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/images-use.html'),
+            'buttonText' => 'Autorizo o uso de imagem'
         ],
     ]
 ];
