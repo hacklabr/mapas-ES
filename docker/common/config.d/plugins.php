b0df43bed2e5823ef7ede58a5cc15fb443405518
erleibiazzio <erleibiazzio@gmail.com>
2023-09-11 17:29:20 -0300

    Instala plugin Zammad

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -2,6 +2,14 @@
 
 return [
     'plugins' => [
-        'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ]
+        'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+        'Zammad' => [
+            'namespace' => 'Zammad',
+            'config' => [
+                'enabled' => true,
+                'url' => 'https://suporte.es.mapasculturais.com.br/assets/chat/chat.min.js',
+                'background' => '#F66968'
+            ]
+        ]
     ]
 ];
