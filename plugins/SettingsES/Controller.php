3b264a73054007b9cbc81ef1548f98816379c85a
Erlei Biazzio <erleibiazzio@gmail.com>
2024-04-18 12:02:32 -0300

    Ajusta endpoint de relatorio LICC para aceitar exibir para qualquer um que tenha controle da oportunidade

--- plugins/SettingsES/Controller.php
+++ plugins/SettingsES/Controller.php
@@ -20,10 +20,6 @@ class Controller extends \MapasCulturais\Controllers\EntityController
         $app = App::i();
 
         $this->requireAuthentication();
-        
-        if (!$app->user->is('admin')) {
-            return;
-        }
 
         $this->render('licc-summary', []);
     }
@@ -34,10 +30,6 @@ class Controller extends \MapasCulturais\Controllers\EntityController
         $app = App::i();
 
         $this->requireAuthentication();
-        
-        if (!$app->user->is('admin')) {
-            return;
-        }
 
         $em = $app->em;
         $conn = $em->getConnection();
@@ -48,6 +40,8 @@ class Controller extends \MapasCulturais\Controllers\EntityController
         if (!isset($request['registrations']) || empty($request['registrations']) || $request['registrations'] == "") {
             $errors[] = "Informe o número de pelomenos uma inscrição";
         }
+
+        $request['registrations'] = trim($request['registrations']);
 
         $delimiter = "\n";
         if (strpos($request['registrations'], ",") !== false) {
@@ -65,37 +59,38 @@ class Controller extends \MapasCulturais\Controllers\EntityController
 
         if ($errors) {
             $this->render('licc-summary-erros', ['errors' => $errors]);
-        }
+        } else {
+            if ($numbers = explode($delimiter, $request['registrations'])) {
+                $results = [];
+                foreach ($numbers as $number) {
+                    $number = trim($number);
+                    $_number = preg_replace('/[^0-9]/i', '', $number);
+                    if ($reg = $app->repo('Registration')->find($_number)) {
+                        if ($reg->opportunity->canUser("@control")) {
+                            if ($revisions = $conn->fetchAll("SELECT * FROM entity_revision er WHERE er.object_type = 'MapasCulturais\Entities\Registration' AND er.object_id = '{$_number}' ORDER BY er.create_timestamp ASC")) {
+                                foreach ($revisions as $revision) {
+                                    $results[$number]['revisions'][] = [
+                                        'data' => (new DateTime($revision['create_timestamp']))->format("d/m/Y H:i:s"),
+                                        'action' => $revision['message'],
+                                    ];
+                                }
+                            }
 
-       
-
-        if ($numbers = explode($delimiter, $request['registrations'])) {
-            $results = [];
-            foreach ($numbers as $number) {
-                $number = trim($number);
-                $_number = preg_replace('/[^0-9]/i', '', $number);
-                
-                if ($revisions = $conn->fetchAll("SELECT * FROM entity_revision er WHERE er.object_type = 'MapasCulturais\Entities\Registration' AND er.object_id = '{$_number}' ORDER BY er.create_timestamp ASC")) {
-                    foreach ($revisions as $revision) {
-                        $results[$number]['revisions'][] = [
-                            'data' => (new DateTime($revision['create_timestamp']))->format("d/m/Y H:i:s"),
-                            'action' => $revision['message'],
-                        ];
+                            if ($files = $conn->fetchAll("SELECT * FROM file f WHERE f.object_type = 'MapasCulturais\Entities\Registration' AND f.object_id = '{$_number}' ORDER BY f.create_timestamp ASC")) {
+                                foreach ($files as $file) {
+                                    $results[$number]['files'][] = [
+                                        'data' => (new DateTime($file['create_timestamp']))->format("d/m/Y H:i:s"),
+                                        'name' => $file['name'],
+                                    ];
+                                }
+                            }
+                        }
                     }
                 }
 
-                if($files = $conn->fetchAll("SELECT * FROM file f WHERE f.object_type = 'MapasCulturais\Entities\Registration' AND f.object_id = '{$_number}' ORDER BY f.create_timestamp ASC")) {
-                    foreach ($files as $file) {
-                        $results[$number]['files'][] = [
-                            'data' => (new DateTime($file['create_timestamp']))->format("d/m/Y H:i:s"),
-                            'name' => $file['name'],
-                        ];
-                    }
+                if ($results) {
+                    $this->render('licc-summary-result', ['results' => $results, 'request' => $request]);
                 }
-            }
-
-            if($results) {
-                $this->render('licc-summary-result', ['results' => $results, 'request' => $request]);
             }
         }
     }
