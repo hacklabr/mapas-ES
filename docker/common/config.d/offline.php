869cc4e98ef5024db6ee91eb968668ad0c6ffc88
Erlei Biazzio <erleibiazzio@gmail.com>
2024-04-24 07:06:37 -0300

    Ajusta intervalo de manutenção

--- docker/common/config.d/offline.php
+++ docker/common/config.d/offline.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'app.offline' => date('Y-m-d H:i:s') < '2024-04-24 09:00:00',
+    'app.offline' => date('Y-m-d H:i:s') > '2024-04-24 08:30:00' && date('Y-m-d H:i:s') < '2024-04-24 09:00:00',
     'app.offlineUrl' => '/em-breve',
     'app.offlineBypassFunction' => function() {
         $senha = $_GET['online'] ?? '';
